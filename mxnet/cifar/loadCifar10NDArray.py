940364cb275bd183e13b44d253ec59370f9150c6
Julien Simon <julien@julien.org>
2020-02-05 11:13:31 +0100

    Update for Python 3

--- mxnet/cifar/loadCifar10NDArray.py
+++ mxnet/cifar/loadCifar10NDArray.py
@@ -2,30 +2,30 @@
 import mxnet as mx 
 import numpy as np
 import cv2
-import cPickle
+import pickle
 import logging
 
 def extractImagesAndLabels(path, file):
     f = open(path+file, 'rb')
-    dict = cPickle.load(f)
-    images = dict['data']
+    dict = pickle.load(f, encoding='bytes')
+    images = dict[b'data']
     images = np.reshape(images, (10000, 3, 32, 32))
-    labels = dict['labels']
+    labels = dict[b'labels']
     imagearray = mx.nd.array(images)
     labelarray = mx.nd.array(labels)
     return imagearray, labelarray
 
 def buildTrainingSet(path):
-    training_data = []
-    training_label = []
+    training_data = np.array([])
+    training_label = np.array([])
     for f in ("data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"):
         imgarray, lblarray = extractImagesAndLabels(path, f)
-        if not training_data:
-            training_data = imgarray
-            training_label = lblarray
-        else:
+        if training_data.size !=0:
             training_data = mx.nd.concatenate([training_data, imgarray])
             training_label = mx.nd.concatenate([training_label, lblarray])
+        else:
+            training_data = imgarray
+            training_label = lblarray
     return training_data, training_label
 
 
@@ -37,8 +37,8 @@ train_iter = mx.io.NDArrayIter(data=training_data,label=training_label,batch_siz
 valid_data, valid_label = extractImagesAndLabels(path, "test_batch")
 valid_iter = mx.io.NDArrayIter(data=valid_data,label=valid_label,batch_size=batch, shuffle=True)
 
-print training_data.shape
-print training_label.shape
-print valid_data.shape
-print valid_label.shape
+print(training_data.shape)
+print(training_label.shape)
+print(valid_data.shape)
+print(valid_label.shape)
 
